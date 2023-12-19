5613cc3bab49c1e863ec42d06afc4fa57fe168a5
ANTONIOS-MAJ-HE <95696217+ANTONIOS-MAJ-HE@users.noreply.github.com>
2023-12-18 19:46:23 -0500

    Update views.py

--- face_face/views.py
+++ face_face/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import Docente, Curso, Asistencia, SesionCurso, Alumno, InscripcionCurso
-from .serializers import AlumnoSerializer, AsistenciaSerializer, CursoSerializer, DocenteSerializer, SesionCursoSerializer, UserSerializer, TokenObtainPairSerializer
+from .serializers import AlumnoSerializer, AsistenciaSerializer, CursoSerializer, DocenteSerializer, SesionCursoSerializer, UserSerializer, TokenObtainPairSerializer, UserRegistrationSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -44,3 +45,16 @@ class SesionCursoViewSet(viewsets.ModelViewSet):
 class AsistenciaViewSet(viewsets.ModelViewSet):
     queryset = Asistencia.objects.all()
     serializer_class = AsistenciaSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = "Successfully registered a new user."
+            data['username'] = account.username
+            data['email'] = account.email
+        else:
+            data = serializer.errors
+        return Response(data, status=status.HTTP_201_CREATED if serializer.is_valid() else status.HTTP_400_BAD_REQUEST)
