9e8dcbe33b58c58c78b6e6348c3db6a762013366
ANTONIOS-MAJ-HE <95696217+ANTONIOS-MAJ-HE@users.noreply.github.com>
2023-12-18 19:47:19 -0500

    Update urls.py

--- face_face/urls.py
+++ face_face/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from face_face import views
-
+from .views import UserRegistrationView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -17,6 +17,7 @@ router.register(r'SesionCurso', views.SesionCursoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api/custom-login/', views.UserViewSet.as_view({'post': 'login'})), # Ruta para el login personalizado
+    path('api/register/', UserRegistrationView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
